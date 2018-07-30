cfd35c1551331894094b00472f323500afb12b09
Bojan Djurkovic <dbojan@gmail.com>
2018-07-29 22:15:39 -0300

    run row details

--- ui/js/components/RunList.vue
+++ ui/js/components/RunList.vue
@@ -1,10 +1,58 @@
 <template>
   <section>
-    <h2 class="subtitle"><strong>Test Runs</strong></h2>
+    <h2 class="subtitle"><strong>Runs</strong></h2>
+     <b-table 
+      :data="data" 
+      :loading="loading"
+
+      paginated
+      backend-pagination
+      :total="total"
+      :per-page="perPage"
+      @page-change="onPageChange"
+
+      :opened-detailed="defaultOpenedDetails"
+      detailed
+      detail-key="id"
+
+      backend-sorting
+      :default-sort-direction="defaultSortOrder"
+      :default-sort="[sortField, sortOrder]"
+      @sort="onSort">
+
+      <template slot-scope="props">
+        <b-table-column field="id" label="ID" width="100" sortable>
+          {{ props.row.id }}
+        </b-table-column>
+
+        <b-table-column field="createdAt" label="Date" width="200" sortable>
+          {{ new Date(props.row.createdAt).toLocaleString() }}
+        </b-table-column>
+
+        <b-table-column field="status" label="Status" centered>
+          <b-icon 
+            :icon="props.row.status === 'ok' ? 'checkbox-marked-circle-outline' : 'alert-circle-outline'" 
+            size="is-medium"
+            custom-size="mdi-24px"
+            :type="props.row.status === 'ok' ? 'is-success' : 'is-danger'"
+          ></b-icon>
+        </b-table-column>
+
+        <b-table-column width="100">
+          <router-link :to="{ name: 'run', params: { projectId: projectId, testId: testId, runId: props.row.id } }" class="button is-info">Details</router-link>
+        </b-table-column>
+      </template>
+
+      <template slot="detail" slot-scope="props">
+        <component-run-details :project-id="projectId" :testId="testId" :runId="props.row.id"></component-run-details>
+      </template>
+    </b-table>
   </section>
 </template>
 
 <script>
+import axios from 'axios'
+import RunRowDetails from './RunRowDetails.vue'
 
 export default {
   data() {
@@ -12,7 +60,7 @@ export default {
       data: [],
       total: 100,
       loading: false,
-      sortField: 'id',
+      sortField: 'createdAt',
       sortOrder: 'desc',
       defaultSortOrder: 'desc',
       page: 1,
@@ -21,8 +69,8 @@ export default {
     }
   },
   props: {
-    projectId: Number,
-    testId: Number
+    projectId: [String, Number],
+    testId: [String, Number]
   },
   watch: {
     projectId(newVal, oldVal) {
@@ -37,7 +85,29 @@ export default {
   },
   methods: {
     async loadData() {
+      const page = this.page - 1 || 0
+      const params = `page=${page}&sort=${this.sortField}&order=${this.sortOrder}`
 
+      this.loading = true
+      try {
+        const { data } = await axios.get(
+          `http://localhost:3000/api/projects/${this.projectId}/tests/${this.testId}/runs?${params}`
+        )
+
+        this.data = data.data
+        this.total = data.total
+        this.loading = false
+      } catch (e) {
+        this.data = []
+        this.total = 0
+        this.loading = false
+
+        this.$snackbar.open({
+          message: e.message,
+          type: 'is-danger',
+          position: 'is-top'
+        })
+      }
     },
 
     onPageChange(page) {
@@ -62,6 +132,9 @@ export default {
     truncate(value, length) {
       return value.length > length ? value.substr(0, length) + '...' : value
     }
+  },
+  components: {
+    'component-run-details': RunRowDetails
   }
 }
 </script>
